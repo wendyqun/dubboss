25e4cdf106a141de7a6a8aa0f63c7808ce542c57
李冠群 <ligq@baojia.com>
2018-11-01 19:48:13 +0800

    dubbo

--- dubboss-server/src/main/java/cn/injava/dubboss/server/config/DubboConfig.java
+++ dubboss-server/src/main/java/cn/injava/dubboss/server/config/DubboConfig.java
@@ -3,8 +3,14 @@ package cn.injava.dubboss.server.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
+/**
+ * @author wangch
+ * @version V1.0
+ * @Package com.dubbo
+ * @Description:
+ * @date 2018/1/27 下午3:36
+ */
 @Configuration
 @ImportResource({ "classpath:dubbo/*.xml" })
 public class DubboConfig {
-
 }
--- dubboss-server/src/main/java/cn/injava/dubboss/server/controller/DemoController.java
+++ dubboss-server/src/main/java/cn/injava/dubboss/server/controller/DemoController.java
@@ -1,35 +1,32 @@
 package cn.injava.dubboss.server.controller;
 
 import cn.injava.dubboss.api.DemoService;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Controller;
+import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * springmvc的controller层
  *
  * Created by apple on 16/2/18.
  */
-@Controller
-public class DemoController implements ApplicationContextAware{
-    ApplicationContext context;
-    @Autowired
-    public DemoService demoProxyService;
+@RestController
+public class DemoController/* implements ApplicationContextAware*/{
+    /*ApplicationContext context;*/
+    @Reference
+    public DemoService demoService;
 
     @ResponseBody
     @RequestMapping("/hello/{value}")
     public String hello(@PathVariable String value){
         //return (((DemoService)context.getBean("demoService")).sayHello(value));
-        return demoProxyService.sayHello(value);
+        return demoService.sayHello(value);
     }
 
-    @Override
+    /*@Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.context=applicationContext;
-    }
+    }*/
 }

7216c84d4b5af4b43605995d95b7399294d2bb41
李冠群 <ligq@baojia.com>
2018-11-17 14:18:07 +0800

    dubbo

--- dubboss-service/src/main/java/cn/injava/dubboss/service/StartService.java
+++ dubboss-service/src/main/java/cn/injava/dubboss/service/StartService.java
@@ -2,6 +2,7 @@ package cn.injava.dubboss.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * 启动服务的提供者
@@ -11,17 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StartService {
     public static void main(String[] args){
-        //System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
-        /*ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("dubbo/dubboss-service.xml") ;
-        ac.start();
-        System.out.println("DemoService服务已经注册成功");
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
         SpringApplication application=new SpringApplication(StartService.class);
-        application.run(args);
+        ConfigurableApplicationContext context=application.run(args);
     }
 }
